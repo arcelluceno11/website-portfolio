0ce55d48f4b5441e8f4bb9440698234c6cc13236
arcel luceno <arcelluceno11@gmail.com>
2023-02-07 17:58:19 +0800

    Updated ..

--- pages/index.jsx
+++ pages/index.jsx
@@ -8,6 +8,7 @@ import {
 } from "react-icons/ai";
 import Image from "next/image";
 import me from "../assets/arcel.png";
+import ParticlesBG from "@/components/ParticlesBG";
 
 export default function Home() {
   return (
@@ -18,59 +19,52 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="bg-white1 px-10">
-        <section id="home" className="min-h-screen">
-          <nav className="py-10 mb-12 flex justify-between">
-            <ul className="flex items-center">
-              <li>
-                <a href="https://https://github.com/arcelluceno11">
-                  <AiFillGithub className="text-xl" />
-                </a>
-              </li>
-              <li>
-                <h1 className="text-xl mx-2">createdByArcel</h1>
-              </li>
-            </ul>
-
-            <ul className="flex items-center">
-              <li>
-                <CiDark className="cursor-pointer text-2xl" />
-              </li>
-              <li>
+      <ParticlesBG />
+      <main id="particles-js" className="absolute px-6 lg:px-10">
+        <section id="home">
+          <div className="px-8 flex flex-col-reverse justify-around items-center lg:flex-row lg:py-0 lg:min-h-screen">
+            <div className="py-2 lg:w-1/2">
+              <h2 className="py-2 text-4xl text-dark text-center lg:text-7xl lg:text-start">
+                Arcel Luceno
+              </h2>
+              <h3 className="py-2 text-xl text-center lg:text-start lg:text-4xl">
+                Software Developer
+              </h3>
+              <p className="py-5 text-lg text-md leading-8 text-center lg:text-start lg:text-xl">
+                A strong understanding of front-end and back-end development,
+                and proficient in multiple programming languages. Extensive
+                expertise in large system architecture development and
+                administration, as well as network design and configuration.
+              </p>
+              <div className="text-5xl flex justify-center gap-12 py-3 text-gray-500 lg:justify-start lg:gap-8">
                 <a
-                  className="px-4 py-2 rounded-md ml-8 bg-dark text-white hover:bg-gray-200 hover:text-dark "
+                  href="https://www.facebook.com/arcelluceno11"
+                  target="_blank"
+                >
+                  <AiFillFacebook className="cursor-pointer hover:text-dark hover:-rotate-3" />
+                </a>
+                <a href="https://www.twitter.com/arcelluceno11" target="_blank">
+                  <AiFillTwitterSquare className="cursor-pointer hover:text-dark hover:-rotate-3" />
+                </a>
+                <a
+                  href="https://https://github.com/arcelluceno11"
+                  target="_blank"
+                >
+                  <AiFillLinkedin className="cursor-pointer hover:text-dark hover:-rotate-3" />
+                </a>
+              </div>
+              <div className="mt-16 flex justify-center items-center lg:justify-start">
+                <a
+                  className="px-4 py-2 rounded-md bg-dark text-lg text-white hover:bg-gray-200 hover:text-dark lg:text-xl"
                   href="#home"
                 >
-                  Contact
+                  Learn More
                 </a>
-              </li>
-            </ul>
-          </nav>
-          <div className="text-center p-10">
-            <h2 className="text-5xl py-2 font-medium text-dark">
-              Arcel Luceno
-            </h2>
-            <h3 className="text-2xl py-2 ">Sofware Developer</h3>
-            <p className="text-2xl py-5 text-md leading-8">
-              A strong understanding of front-end and back-end development, and
-              proficient in multiple programming languages. Extensive expertise
-              in large system architecture development and administration, as
-              well as network design and configuration.
-            </p>
-          </div>
-          <div className="text-5xl flex justify-center gap-12 py-3 text-gray-500 ">
-            <a href="https://www.facebook.com/arcelluceno11" target="_blank">
-              <AiFillFacebook className="cursor-pointer hover:text-dark" />
-            </a>
-            <a href="https://www.twitter.com/arcelluceno11" target="_blank">
-              <AiFillTwitterSquare className="cursor-pointer hover:text-dark" />
-            </a>
-            <a href="https://https://github.com/arcelluceno11" target="_blank">
-              <AiFillLinkedin className="cursor-pointer hover:text-dark" />
-            </a>
-          </div>
-          <div className="mx-auto bg-gradient-to-b from-dark to-white rounded-full w-80 h-80 mt-16 overflow-hidden">
-            <Image src={me} />
+              </div>
+            </div>
+            <div className="w-45 h-45 mt-10 mb-2 lg:m-0 mx-auto bg-gradient-to-b from-dark to-white rounded-full overflow-hidden lg:h-1/2">
+              <Image src={me}/>
+            </div>
           </div>
         </section>
       </main>
